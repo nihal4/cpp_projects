efec292ad0ac3b85092ce9bf380809092d3b21ec
nihal4 <sm.nihal4@gmail.com>
2023-03-04 18:46:17 +0600

    done

--- calculator.cpp
+++ calculator.cpp
@@ -5,22 +5,37 @@ using namespace std;
 
 class Calculator{
     public :
-        int input_one,input_two;
+        float input_one,input_two;
         char add,substraction,multiplication,devide ;
-        Calculator(int a_input_one , char option,int a_input_two ){
+        Calculator(float a_input_one , char option,float a_input_two ){
             input_one = a_input_one;
             input_two = a_input_two;
-            add = option ;
+            add = option;
+            substraction = option;
+            multiplication = option;
+            devide = option ;
         }
-        char calculate(){
+        void calculate(){
             if(add == '+'){
                 cout << input_one + input_two << endl;
-            }else {cout << 'ok' << endl;}
+            }else if(substraction == '-'){
+                cout << input_one - input_two << endl;
+            }else if(multiplication == 'x'){
+                cout << input_one * input_two << endl;
+            }else if(devide == '/'){
+                cout << input_one / input_two << endl;
+            }else{
+                cout << "ok" << endl;
+            }
         }
 };
 
 
 int main(){
-    Calculator cal1(5,'+',6);
+    float input1 , input2 ; char chose;
+    cout << "number1 : ";cin >> input1;
+    cout << "+,-,x,/ : ";cin >> chose;
+    cout << "number2 : ";cin >> input2;
+    Calculator cal1(input1,chose,input2);
     cal1.calculate();
 }
